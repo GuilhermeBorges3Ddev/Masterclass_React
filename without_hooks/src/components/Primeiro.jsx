9055edd582c2fdbdcc02eddea94167af10eb7c9a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-09-19 18:56:41 -0300

    Simplest component did here, with only arrow and text inside

---
+++ without_hooks/src/components/Primeiro.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default () => <>
+                        <h1>Primeiro componente (Minimalista, via arrow)</h1>
+                        <h2>Exemplo de um componente</h2>
+                     </>
---
+++ without_hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+import Primeiro from './components/Primeiro';
+
+ReactDOM.render(
+    <Primeiro />,
+    document.getElementById('root')
+)
